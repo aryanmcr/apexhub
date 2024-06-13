84a7380ab8c99e1a29a948734897e99ce5ad4bd9
aryan <152167113+aryanmcr@users.noreply.github.com>
2024-06-13 19:58:31 +0100

    Update bss.lua

--- bss.lua
+++ bss.lua
@@ -19,9 +19,9 @@ end
 -- Load APIs
 local api = loadstring(game:HttpGet('https://raw.githubusercontent.com/aryanmcr/apexhub/main/api.lua'))()
 
-local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
+local repo = 'https://github.com/violin-suzutsuki/LinoriaLib/main/'
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/NatsuDevs/Linoria-Rewrite-Modded-/main/Lib.lua"))()
+local Library = loadstring(game:HttpGet('repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
